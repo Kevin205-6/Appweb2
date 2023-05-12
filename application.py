56bdecf766bf7cd940c002980342382d39c29028
Kevin Pérez <perez.kevin.1cv.2017@gmail.com>
2023-05-12 16:40:45 -0600

    mod in2

--- application.py
+++ application.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template 
+#Exportar las biblotecas (flask y una que le pertece a flask  "render_template"
+#que permite llamar un archivo index desde una carpeta llamada templates
 
-app = Flask(__name__)
+app = Flask(__name__) #Iniciar flask en una variable
 
-@app.route('/')
-def index():
-    return render_template('index.html');
+@app.route('/') #Esto permite que cualquier archivo se llame dentro del documento desde la raiz del proyecto
+def index(): #Crea un metodo para que te retorne una pagina web con html
+    return render_template('index.html'); #Forma de retornar el archivo html desde la carpeta llamada templates
 
-if __name__ == '__main__':
+if __name__ == '__main__': #Si el programa se ejecuta en forma de main te inicia una aplicación de python
     app.run(debug=True)
